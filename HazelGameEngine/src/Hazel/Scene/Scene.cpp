ca0774510b8cd2d03556cffada21ceba10d447d3
ohbumjun <beomjun0638@hanmail.net>
2023-09-09 20:19:38 +0900

    feat(Engine) Add Basic tutorial code for using Entt library

--- HazelGameEngine/src/Hazel/Scene/Scene.cpp
+++ HazelGameEngine/src/Hazel/Scene/Scene.cpp
@@ -1,7 +1,60 @@
 #include "hzpch.h"
 #include "Scene.h"
+#include <glm/glm.hpp>
 
 namespace Hazel
 {
+	static void OnTransformConstruct() {};
 
+	Practice::Practice()
+	{
+		struct MeshComponent{};
+		struct TransformComponent
+		{
+			glm::mat4 transform;
+			TransformComponent() = default;
+			TransformComponent(const TransformComponent&) = default;
+			TransformComponent(const glm::mat4& trans) :
+				transform(trans){}
+		
+			operator const glm::mat4& () const { return transform; }
+			operator glm::mat4&() { return transform; }
+		};
+
+		entt::entity entity = m_Registry.create();
+		m_Registry.emplace<TransformComponent>(entity, glm::mat4(1.f));
+
+		// Transform Component 생성 때마다 해당 함수 호출
+		m_Registry.on_construct<TransformComponent>().connect<&OnTransformConstruct>();
+		
+		if (m_Registry.try_get<TransformComponent>(entity))
+		{
+			TransformComponent& transform = m_Registry.get<TransformComponent>(entity);
+		}
+
+		// TransformComponent  를 가진 모든 entity 리턴
+		auto view = m_Registry.view<TransformComponent>();
+
+		for (const auto& entity : view)
+		{
+			TransformComponent& transform1 = m_Registry.get<TransformComponent>(entity);
+			TransformComponent& transform2 = view.get<TransformComponent>(entity);
+		}
+
+		auto group = m_Registry.group<TransformComponent>();
+
+		for (const auto& entity : group)
+		{
+			auto& [transform, mesh] = group.get<TransformComponent, MeshComponent>(entity);
+
+		}
+	}
+
+	Scene::Scene()
+	{
+	}
+	Scene::~Scene()
+	{
+	}
+	
 }
--- HazelGameEngine/src/Hazel/Scene/Scene.h
+++ HazelGameEngine/src/Hazel/Scene/Scene.h
@@ -4,6 +4,13 @@
 
 namespace Hazel
 {
+	class Practice
+	{
+		Practice();
+
+		entt::registry m_Registry;
+	};
+
 	class Scene
 	{
 	public :
@@ -13,6 +20,7 @@ namespace Hazel
 	private :
 		/*
 		* ecs 내 모든 component data + entity id 정보를 담는 container
+		* entity : component  들이 어디에 속하는지에 대한 id 정보일 뿐이다.
 		*/
 		entt::registry m_Registry;
 	};
