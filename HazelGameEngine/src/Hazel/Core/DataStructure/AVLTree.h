20e3eddaa88214101041fbe744fd114284c7aa76
ohbumjun <beomjun0638@hanmail.net>
2023-10-28 15:32:10 +0900

    feat(Engine) Add DoublyLinkedList DS && Fix AVLTree To allow duplicate key

--- HazelGameEngine/src/Hazel/Core/DataStructure/AVLTree.h
+++ HazelGameEngine/src/Hazel/Core/DataStructure/AVLTree.h
@@ -1,4 +1,5 @@
-#pragma once
+﻿#pragma once
+#include <cassert>
 
 template <typename KEY, typename VALUE>
 class AVLTreeNode
@@ -23,11 +24,15 @@ private:
 	}
 
 private:
-	AVLTreeNode<KEY, VALUE>* m_Left;
-	AVLTreeNode<KEY, VALUE>* m_Right;
-	AVLTreeNode<KEY, VALUE>* m_Parent;
-	AVLTreeNode<KEY, VALUE>* m_Next;
-	AVLTreeNode<KEY, VALUE>* m_Prev;
+	AVLTreeNode<KEY, VALUE>* m_Left = nullptr;
+	AVLTreeNode<KEY, VALUE>* m_Right = nullptr;
+	AVLTreeNode<KEY, VALUE>* m_Parent = nullptr;
+	AVLTreeNode<KEY, VALUE>* m_Next = nullptr;
+	AVLTreeNode<KEY, VALUE>* m_Prev = nullptr;
+
+	// 같은 KEY 가 들어올 경우, 연결리스트 형태로 모아둔다.
+	AVLTreeNode<KEY, VALUE>* m_NextInList = nullptr;
+	AVLTreeNode<KEY, VALUE>* m_PrevInList = nullptr;
 
 public:
 	KEY		first;
@@ -186,7 +191,7 @@ public:
 
 
 public:
-	void insert(const KEY& key, const VALUE& value)
+	void Insert(const KEY& key, const VALUE& value)
 	{
 		// 처음 데이터를 추가할 경우
 		if (!m_Root)
@@ -212,36 +217,39 @@ public:
 		++m_Size;
 	}
 
-	int size()	const
+	int Size()	const
 	{
 		return m_Size;
 	}
 
-	bool empty()	const
+	bool Empty()	const
 	{
 		return m_Size == 0;
 	}
 
-	iterator begin()	const
+	iterator Begin()	const
 	{
 		iterator	iter;
 		iter.m_Node = m_Begin->m_Next;
 		return iter;
 	}
 
-	iterator end()	const
+	iterator End()	const
 	{
 		iterator	iter;
 		iter.m_Node = m_End;
 		return iter;
 	}
 
-	iterator Find(const KEY& key)	const
+	// 원래는 key 로만 찾았었는데, 
+	// 중복 key 를 허용하는 정책을 사용하기 때문에, value 도 같이 사용하여
+	// 원하는 데이터를 찾는 로직을 적용한다.
+	iterator Find(const KEY& key, const VALUE& value)	const
 	{
-		return Find(key, m_Root);
+		return find(key, value,  m_Root);
 	}
 
-	void clear()
+	void Clear()
 	{
 		PNODE	Node = m_Begin->m_Next;
 
@@ -284,26 +292,49 @@ public:
 		PostOrder(pFunc, m_Root);
 	}
 
-	iterator erase(const KEY& key)
+	// 같은 key 를 제거하고, 그 연결리스트 상에서 만약 첫번째 노드가 되었다면
+	// m_Right, m_Left 등의 정보들을 다시 세팅해줘야 한다.
+	iterator Erase(const KEY& key, const VALUE& value)
 	{
-		iterator	iter = Find(key);
+		iterator	iter = Find(key, value);
 
-		if (iter == end())
+		if (iter == End())
 			return iter;
 
-		return erase(iter);
+		return Erase(iter);
 	}
 
-	iterator erase(const iterator& iter)
+	iterator Erase(const iterator& iter)
 	{
-		if (iter == end())
+		if (iter == End())
 			return iter;
 
 		else if (iter == m_Begin)
-			return end();
+			return End();
 
 		else if (iter.m_Node == nullptr)
-			return end();
+			return End();
+
+		PNODE returnNode = nullptr;
+
+		bool firstNodeInList = isFirstInSameKeyList(iter.m_Node);
+
+		// list 상에서 유일한 노드가 아니라면
+		// 그냥 list 상에서 지워주고 끝낸다.
+		if (isSingleNodeInList(iter.m_Node) == false)
+		{
+			PNODE deleteNextNodeInList = getNextNodeInList(iter.m_Node);
+			returnNode = deleteNextNodeInList;
+			eraseInSameKeyList(iter.m_Node);
+
+			delete	iter.m_Node;
+
+			--m_Size;
+
+			iterator result;
+			result.m_Node = returnNode;
+			return result;
+		}
 
 		// 리프노드일 경우 부모로부터 연결을 제거하고 노드를 제거해준다.
 		if (!iter.m_Node->m_Left && !iter.m_Node->m_Right)
@@ -316,37 +347,57 @@ public:
 			// 것이다.
 			if (!Parent)
 			{
+				// 여기로 들어왔다는 것은
+				// 1) root node 이면서
+				// 2) list 상에서 자기 혼자라는 의미이다.
+
 				delete	iter.m_Node;
 
-				m_Root = nullptr;
 				--m_Size;
+
+				m_Root = nullptr;
 
 				// 다 지웠으니 Begin과 End끼리 연결한다.
 				m_Begin->m_Next = m_End;
 				m_End->m_Prev = m_Begin;
 
-				return end();
+				return End();
 			}
+
+			PNODE nextNodeInList = getNextNodeInList(iter.m_Node);
 
 			// 지우려는 노드가 부모느드의 왼쪽 노드인지 오른쪽 노드인지를
 			// 판단하여 부모와의 연결을 끊어준다.
 			if (Parent->m_Left == iter.m_Node)
-				Parent->m_Left = nullptr;
-
+			{
+				// Parent->m_Left = nullptr;
+				Parent->m_Left = nextNodeInList;
+			}
 			else
-				Parent->m_Right = nullptr;
+			{
+				// Parent->m_Right = nullptr;
+				Parent->m_Right = nextNodeInList;
+			}
 
 			PNODE	Prev = iter.m_Node->m_Prev;
 			PNODE	Next = iter.m_Node->m_Next;
 
-			Prev->m_Next = Next;
-			Next->m_Prev = Prev;
+			if (nextNodeInList)
+			{
+				Prev->m_Next = nextNodeInList;
+				Next->m_Prev = nextNodeInList;
+			}
+			else
+			{
+				Prev->m_Next = Next;
+				Next->m_Prev = Prev;
+			}
 
 			delete	iter.m_Node;
 
 			--m_Size;
 
-			ReBalance(Parent);
+			reBalance(Parent);
 
 
 			// 지운 노드의 다음노드를 넣어서 iterator를 반환해준다.
@@ -362,6 +413,24 @@ public:
 		{
 			// 왼쪽에 존재하는 노드들중 가장 큰 노드를 찾아온다.
 			PNODE	MaxNode = FindMax(iter.m_Node->m_Left);
+
+			PNODE maxNextNodeInList = getNextNodeInList(MaxNode);
+
+			// 여기로 들어온 다는 것은, iter.m_Node 가 List 상에서
+			// 첫번째 노드라는 것이다.
+			// 즉, 기존에 자기가 속한 list 상에서 위치 및 관계 정보를
+			// updaet 할 필요는 없다.
+			// 그저 새로 값이 세팅됨에 따라, 새로운 list 에
+			// 해당 노드를 넣어주면 되는 것이다.
+
+			assert(iter.m_Node->m_NextInList == nullptr);
+
+			iter.m_Node->m_NextInList = maxNextNodeInList;
+
+			if (maxNextNodeInList)
+			{
+				maxNextNodeInList->m_PrevInList = iter.m_Node;
+			}
 
 			// 찾아준 노드의 Key, Value 값으로 변경해준다.
 			iter.m_Node->first = MaxNode->first;
@@ -390,14 +459,16 @@ public:
 			Prev->m_Next = Next;
 			Next->m_Prev = Prev;
 
+			returnNode = Next;
+
+			reBalance(Parent);
+
 			delete	MaxNode;
 
 			--m_Size;
 
-			ReBalance(Parent);
-
 			iterator	result;
-			result.m_Node = Next;
+			result.m_Node = returnNode;
 
 			return result;
 		}
@@ -405,6 +476,17 @@ public:
 		// 지울 노드의 오른쪽 노드만 존재할 경우 오른쪽 노드에서 가장 작은 노드를
 		// 찾아온다.
 		PNODE	MinNode = FindMin(iter.m_Node->m_Right);
+
+		PNODE minNextNodeInList = getNextNodeInList(MinNode);
+
+		assert(iter.m_Node->m_NextInList == nullptr);
+
+		iter.m_Node->m_NextInList = minNextNodeInList;
+
+		if (minNextNodeInList)
+		{
+			minNextNodeInList->m_PrevInList = iter.m_Node;
+		}
 
 		// 찾아준 노드의 Key, Value 값으로 변경해준다.
 		iter.m_Node->first = MinNode->first;
@@ -433,23 +515,80 @@ public:
 		Prev->m_Next = Next;
 		Next->m_Prev = Prev;
 
+		returnNode = Next;
+
+		reBalance(Parent);
+
 		delete	MinNode;
 
 		--m_Size;
 
-		ReBalance(Parent);
 
 		iterator	result;
-		result.m_Node = Next;
+		result.m_Node = returnNode;
 
 		return result;
+	}
+
+	template<typename Function>
+	void Find(Function fn)
+	{
+		PNODE findNode = m_Root;
+
+		while (findNode)
+		{
+			int result = fn(findNode);
+
+			// 원하는 값을 찾음
+			if (result == 0) break;
+
+			else if (result < 0)
+				findNode = findNode->m_Left;
+			else
+				findNode = findNode->m_Right;
+		}
+	}
+
+	PNODE FindMax(PNODE Node)
+	{
+		if (Node->m_Right)
+			return FindMax(Node->m_Right);
+
+		return Node;
+	}
+
+	PNODE FindMin(PNODE Node)
+	{
+		if (Node->m_Left)
+			return FindMin(Node->m_Left);
+
+		return Node;
 	}
 
 private:
 	PNODE insert(const KEY& key, const VALUE& value, PNODE ParentNode)
 	{
+		// 같은 크기의 key 가 들어올 수도 있다. 이 경우는 어떻게 처리할 것인가
+		if (ParentNode->first == key)
+		{
+			// 같은 key 들은 연결리스트 형태로 묶어둔다.
+			PNODE lastNodeInList = ParentNode;
+
+			while (lastNodeInList->m_NextInList) lastNodeInList = lastNodeInList->m_NextInList;
+
+			PNODE	NewNode = new NODE;
+
+			NewNode->first = key;
+			NewNode->second = value;
+			NewNode->m_PrevInList = lastNodeInList;
+
+			lastNodeInList->m_NextInList = NewNode;
+
+			return NewNode;
+		}
+
 		// 기준노드보다 작다면 왼쪽이다.
-		if (ParentNode->first > key)
+		else if (ParentNode->first > key)
 		{
 			// 만약 기준노드의 왼쪽 자식노드가 있다면 그 왼쪽 자식노드를
 			// 기준노드로 하여 다시 탐색을 하게 한다.
@@ -475,10 +614,10 @@ private:
 			Prev->m_Next = NewNode;
 			NewNode->m_Prev = Prev;
 
-			NewNode->m_Next = Node;
+			NewNode->m_Next = ParentNode;
 			ParentNode->m_Prev = NewNode;
 
-			ReBalance(NewNode);
+			reBalance(NewNode);
 
 			return NewNode;
 		}
@@ -486,8 +625,8 @@ private:
 		// 여기로 내려오면 값이 크다는 것이므로 오른쪽으로 탐색을 해봐야 한다.
 		// 만약 오른쪽 자식노드가 있을 경우 기준노드를 오른쪽 자식노드로 하여
 		// 탐색하게 한다.
-		if (Node->m_Right)
-			return insert(key, value, Node->m_Right);
+		if (ParentNode->m_Right)
+			return insert(key, value, ParentNode->m_Right);
 
 		// 더이상 오른쪽 자식노드가 없을 경우 이 위치에 새로 노드를 생성하여
 		// 추가해주어야 한다.
@@ -497,44 +636,60 @@ private:
 		NewNode->second = value;
 
 		// 기준노드의 오른쪽 자식노드로 지정한다.
-		Node->m_Right = NewNode;
-		NewNode->m_Parent = Node;
+		ParentNode->m_Right = NewNode;
+		NewNode->m_Parent = ParentNode;
 
-		PNODE	Next = Node->m_Next;
+		PNODE	Next = ParentNode->m_Next;
 
-		Node->m_Next = NewNode;
-		NewNode->m_Prev = Node;
+		ParentNode->m_Next = NewNode;
+		NewNode->m_Prev = ParentNode;
 
 		NewNode->m_Next = Next;
 		Next->m_Prev = NewNode;
 
-		ReBalance(NewNode);
+		reBalance(NewNode);
 
 		return NewNode;
 	}
 
-	iterator Find(const KEY& key, PNODE Node)	const
+	iterator find(const KEY& key, const VALUE& value, PNODE Node)	const
 	{
 		// 기준노드가 nullptr일 경우 더이상 탐색할 노드가 없으므로
 		// end를 리턴한다. 못찾았다는 것이다.
 		if (!Node)
-			return end();
+			return End();
 
 		if (Node->first == key)
 		{
-			iterator	iter;
-			iter.m_Node = Node;
-			return iter;
+			// 같은 key 를 모아둔 연결리스트를 뒤져서 원하는 데이터를 찾는다.
+			PNODE searchNode = Node;
+			
+			while (searchNode)
+			{
+				if (searchNode->second == value)
+				{
+					iterator	iter;
+					iter.m_Node = searchNode;
+					return iter;
+				}
+
+				searchNode = searchNode->m_NextInList;
+			}
+
+			// 여기에 걸리면 해당 key 로 된 value 가 존재하지 않는다는 것이다
+			assert(false);
 		}
 
 		// 키를 비교하여 작으면 왼쪽, 크면 오른쪽으로 탐색해서 들어간다.
 		if (Node->first > key)
-			return Find(key, Node->m_Left);
+		{
+			return find(key, value, Node->m_Left);
+		}
 
-		return Find(key, Node->m_Right);
+		return find(key, value, Node->m_Right);
 	}
 
-	void PreOrder(void(*pFunc)(const KEY&, const VALUE&), PNODE Node)
+	void preOrder(void(*pFunc)(const KEY&, const VALUE&), PNODE Node)
 	{
 		if (!Node)
 			return;
@@ -544,7 +699,7 @@ private:
 		PreOrder(pFunc, Node->m_Right);
 	}
 
-	void InOrder(void(*pFunc)(const KEY&, const VALUE&), PNODE Node)
+	void inOrder(void(*pFunc)(const KEY&, const VALUE&), PNODE Node)
 	{
 		if (!Node)
 			return;
@@ -554,7 +709,7 @@ private:
 		InOrder(pFunc, Node->m_Right);
 	}
 
-	void PostOrder(void(*pFunc)(const KEY&, const VALUE&), PNODE Node)
+	void postOrder(void(*pFunc)(const KEY&, const VALUE&), PNODE Node)
 	{
 		if (!Node)
 			return;
@@ -564,15 +719,7 @@ private:
 		pFunc(Node->first, Node->second);
 	}
 
-	PNODE FindMax(PNODE Node)
-	{
-		if (Node->m_Right)
-			return FindMax(Node->m_Right);
-
-		return Node;
-	}
-
-	PNODE RotationLeft(PNODE Node)
+	PNODE rotationLeft(PNODE Node)
 	{
 		// 기준노드의 부모노드를 얻어온다.
 		PNODE	Parent = Node->m_Parent;
@@ -622,7 +769,7 @@ private:
 		return RightChild;
 	}
 
-	PNODE RotationRight(PNODE Node)
+	PNODE rotationRight(PNODE Node)
 	{
 		// 기준노드의 부모노드를 얻어온다.
 		PNODE	Parent = Node->m_Parent;
@@ -670,21 +817,13 @@ private:
 		return LeftChild;
 	}
 
-	PNODE FindMin(PNODE Node)
-	{
-		if (Node->m_Left)
-			return FindMin(Node->m_Left);
-
-		return Node;
-	}
-
-	int GetHeight(PNODE Node)
+	int getHeight(PNODE Node)
 	{
 		if (!Node)
 			return 0;
 
-		int	Left = GetHeight(Node->m_Left);
-		int	Right = GetHeight(Node->m_Right);
+		int	Left = getHeight(Node->m_Left);
+		int	Right = getHeight(Node->m_Right);
 
 		int	Height = Left > Right ? Left : Right;
 
@@ -692,31 +831,41 @@ private:
 		return Height + 1;
 	}
 
-	int BalanceFactor(PNODE Node)
+	int balanceFactor(PNODE Node)
 	{
-		return GetHeight(Node->m_Left) - GetHeight(Node->m_Right);
+		return getHeight(Node->m_Left) - getHeight(Node->m_Right);
 	}
 
-	void ReBalance(PNODE Node)
+	bool isFirstInSameKeyList(PNODE Node)
+	{
+		return Node->m_PrevInList ? false : true;
+	}
+
+	bool isSingleNodeInList(PNODE Node)
+	{
+		return (Node->m_PrevInList || Node->m_NextInList) ? false : true;
+	}
+
+	void reBalance(PNODE Node)
 	{
 		if (!Node)
 			return;
 
 		// 왼쪽과 오른쪽의 높이차이를 구해준다.
 		// 왼쪽 높이 - 오른쪽 높이
-		int	Factor = BalanceFactor(Node);
+		int	Factor = balanceFactor(Node);
 
 		// 오른쪽으로 균형이 무너졌을 경우
 		if (Factor <= -2)
 		{
-			int	RightFactor = BalanceFactor(Node->m_Right);
+			int	RightFactor = balanceFactor(Node->m_Right);
 
 			// RR
 			// 음수라면 오른쪽 오른쪽으로 균형이 무너졌을 경우이다.
 			if (RightFactor <= 0)
 			{
 				// 기준노드를 중심으로 왼쪽으로 회전을 시켜준다.
-				Node = RotationLeft(Node);
+				Node = rotationLeft(Node);
 			}
 
 			// RL => 1) R 회전 2) L 회전
@@ -724,37 +873,115 @@ private:
 			else
 			{
 				// 오른쪽 자식노드를 중심으로 오른쪽 회전을 한다.
-				RotationRight(Node->m_Right);
+				rotationRight(Node->m_Right);
 
 				// 기준노들를 중심으로 왼쪽 회전을 시켜준다.
-				Node = RotationLeft(Node);
+				Node = rotationLeft(Node);
 			}
 		}
 
 		// 왼쪽으로 균형이 무너졌을 경우
 		else if (Factor >= 2)
 		{
-			int	LeftFactor = BalanceFactor(Node->m_Left);
+			int	LeftFactor = balanceFactor(Node->m_Left);
 
 			// 왼쪽 왼쪽으로 균형이 무너졌을 경우
 			if (LeftFactor >= 0)
 			{
 				// 기준노드를 중심으로 오른쪽으로 회전한다.
-				Node = RotationRight(Node);
+				Node = rotationRight(Node);
 			}
 
 			// 왼쪽 오른쪽으로 균형이 무너졌을 경우
 			else
 			{
 				// 기준노드의 왼쪽 자식노드를 중심으로 왼쪽으로 회전한다.
-				RotationLeft(Node->m_Left);
+				rotationLeft(Node->m_Left);
 
 				// 기준노드를 중심으로 오른쪽으로 회전한다.
-				Node = RotationRight(Node);
+				Node = rotationRight(Node);
 			}
 		}
 
-		ReBalance(Node->m_Parent);
+		reBalance(Node->m_Parent);
+	}
+
+	PNODE getNextNodeInList(PNODE Node)
+	{
+		return Node->m_NextInList;
+	}
+
+	// 같은 Key 를 가진  List 상에서 해당 Node 정보를 지워주는 함수
+	void eraseInSameKeyList(PNODE Node)
+	{
+		bool isFirstNodeInList = Node->m_PrevInList ? false : true;
+
+		PNODE prevNodeInList = Node->m_PrevInList;
+		PNODE nextNodeInList = Node->m_NextInList;
+
+		// Node 가 현재 List 상에서 첫번째 Node 일 수 있다.
+		if (prevNodeInList)
+		{
+			prevNodeInList->m_NextInList = nextNodeInList;
+		}
+
+		// List 상에서 마지막 노드일 수도 있다.
+		if (nextNodeInList)
+		{
+			nextNodeInList->m_PrevInList = prevNodeInList;
+		}
+
+		// 그 다음 m_Next, m_Prev 등 정보도 다시 세팅해줄 것이다.
+		if (isFirstNodeInList)
+		{
+			// nextNode 가 이제 list 상의 첫번째 노드가 되는 것이다.
+			PNODE nextNode   = Node->m_Next;
+			PNODE prevNode   = Node->m_Prev;
+			PNODE leftNode		 = Node->m_Left;
+			PNODE rightNode   = Node->m_Right;
+			PNODE parentNode = Node->m_Parent;
+
+			if (!parentNode)
+			{
+				// Node 가 Root Node 였다는 의미이다.
+				m_Root = nextNodeInList;
+				m_Begin->m_Next = m_End;
+				m_End->m_Prev = m_Begin;
+			}
+			else
+			{
+				// 부모 노드가 있다면
+				if (parentNode->m_Left == Node)
+					parentNode->m_Left = nextNodeInList;
+				else if (parentNode->m_Right == Node)
+					parentNode->m_Right = nextNodeInList;
+				else
+				{
+					// 여기에 걸리면 무조건 Crash
+					assert(false);
+				}
+
+				nextNodeInList->m_Parent = parentNode;
+			}
+
+			prevNode->m_Next = nextNodeInList;
+			nextNodeInList->m_Prev = prevNode;
+
+			nextNode->m_Prev = nextNodeInList;
+			nextNodeInList->m_Next = nextNode;
+
+			nextNodeInList->m_Left = leftNode;
+			if (leftNode)
+			{
+				leftNode->m_Parent = nextNodeInList;
+			}
+
+			nextNodeInList->m_Right = rightNode;
+			if (rightNode)
+			{
+				rightNode->m_Parent = nextNodeInList;
+			}
+		}
 	}
 private:
 	PNODE	m_Root;
