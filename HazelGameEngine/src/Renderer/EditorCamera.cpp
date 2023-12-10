02d7cfe14a6d25f6d634b6bda9340778579703b7
ohbumjun <beomjun0638@hanmail.net>
2023-12-10 20:43:13 +0900

    feat(Editor) Analyzing Editor Camera Logic Complete

--- HazelGameEngine/src/Renderer/EditorCamera.cpp
+++ HazelGameEngine/src/Renderer/EditorCamera.cpp
@@ -26,11 +26,13 @@ namespace Hazel {
 	}
 
 	void EditorCamera::UpdateView()
-	{
+	{ 
 		// m_Yaw = m_Pitch = 0.0f; // Lock the camera's rotation
 		m_Position = CalculatePosition();
 
 		glm::quat orientation = GetOrientation();
+
+		// S R T -> S 는 없으니 제외 -> 카메라의 World 변환 행렬
 		m_ViewMatrix = glm::translate(glm::mat4(1.0f), m_Position) * glm::toMat4(orientation);
 		m_ViewMatrix = glm::inverse(m_ViewMatrix);
 	}
@@ -66,7 +68,17 @@ namespace Hazel {
 		if (Input::IsKeyPressed(Key::LeftAlt))
 		{
 			const glm::vec2& mouse{ Input::GetMouseX(), Input::GetMouseY() };
+
+			/*
+			마우스 
+			왼 -> 오 : delta.x 가 0 보다 크다 
+			오 -> 왼 : delta.x 가 0 보다 작다
+
+			위 -> 아래 : delta.y 가 양수값
+			아래 -> 위 : delta.y 가 음수값
+			*/
 			glm::vec2 delta = (mouse - m_InitialMousePosition) * 0.003f;
+
 			m_InitialMousePosition = mouse;
 
 			if (Input::IsMouseButtonPressed(Mouse::ButtonMiddle))
@@ -103,24 +115,98 @@ namespace Hazel {
 
 	void EditorCamera::MouseRotate(const glm::vec2& delta)
 	{
+		/*
+		GetUpDirection().y  == 0 ?
+		== 카메라가 수직 위를 보고 있다는 의미
+		
+		GetUpDirection().y < 0 ?
+		> 앞쪽을 바라보고 있다는 것
+
+		GetUpDirection().y < 0 ?
+		> 몸을 뒤로 제껴서 뒤쪽을 바라보고 있다는 것
+
+		(참고 링크)
+		https://www.notion.so/OpenGL-Hazel-0415bd834e3c481ea358e9eb08ca52e5
+
+		"마우스 기준"
+
+		여기서 중요한 것은, 카메라가 자기 축을 가지고 혼자 
+		상하 좌우 기준으로 회전하는 것이 아니다.
+		즉, 회전 축의 원점이, 카메라 위치가 아니라
+		m_Focal Point 이다.
+
+		예시 1) 마우스 오른쪽 -> 왼쪽
+		- delta.x < 0 
+		- 카메라가 오른쪽으로, 우측으로 회전하는 개념이 아니다
+		- 앞에 박스가 있으면, m_Focal Point 기준으로 오른쪽 방향으로 
+		  돌아가면서, 박스의 우측을 보는 것이다.
+		- 그 말은 사실, 카메라가 정면이 아니라, 점차 왼쪽으로 회전한다는 것을
+		  의미한다.
+		
+		자. 그런데 여기서 조건이 하나 더 붙는다.
+		1_1) 지금 앞쪽을 바라보고 있었거나
+		- 이 상태에서 카메라 왼쪽 회전은, world 의 y 축 왼쪽 회전이다.
+		  y 축 왼쪽 회전은, 반시계방향 회전이고
+		  오른손 좌표계에서 이는 양수값 회전을 의미한다.
+		- 따라서 Yaw 에 양수값을 더하는 것이다.
+
+		1_2) 몸을 제껴서 뒤쪽을 사실상 바라보고 있었거나
+		(고개를 들다가 바로 위보다 더 뒤쪽을 바라보는 것)
+
+		- 반대로 이 경우는 사실 world y 축 기준 오른쪽 회전과 같다.
+		  시계 방향 회전이고
+		  이는 오른손 좌표계 에서, Yaw 에 음수값을 더하는 것이다.
+
+		예시 2) 마우스 아래 -> 위
+		- delta.y 가 음수값이다. 작은 값에서 큰 값으로 간 것.
+		실제 프로젝트를 실행하면, 카메라가 점차 정면 Box 의 아랫면을 바라보기
+		시작한다.
+		바로 앞에서 아랫면을 본다는 것은
+
+		사실 카메라 입장에서는, 점점 위쪽을 바라본다는 것이다.
+		Focal Point 는 World 좌표계 (0,0,0) 으로 고정되지만
+
+		카메라는 사실 점차 위쪽을 향하게 된다는 것이다.
+
+		위쪽을 향한다는 것은, X 축 기준 반시계 방향 회전인 것이고
+		이는 오른손 좌표계에서 양수값 회전이다.
+
+		따라서 Pitch 에 양수값을 더하는 것이다.
+		*/
+
 		float yawSign = GetUpDirection().y < 0 ? -1.0f : 1.0f;
+
 		m_Yaw += yawSign * delta.x * RotationSpeed();
+
 		m_Pitch += delta.y * RotationSpeed();
+
+		/*
+		glm::vec3 forwardVec = GetForwardDirection();
+		HZ_CORE_INFO("Camera For Vector {0} {1} {2}", forwardVec.x, forwardVec.y, forwardVec.z);
+		*/
+
+		/*
+		glm::vec3 orientVec = glm::vec3(-m_Pitch, -m_Yaw, 0.0f);
+		HZ_CORE_INFO("Camera For Vector {0} {1} {2}", orientVec.x, orientVec.y, orientVec.z);
+		*/
 	}
 
 	void EditorCamera::MouseZoom(float delta)
 	{
 		m_Distance -= delta * ZoomSpeed();
+
 		if (m_Distance < 1.0f)
 		{
-			m_FocalPoint += GetForwardDirection();
+			glm::vec3 forwardVec = GetForwardDirection();
+			m_FocalPoint += forwardVec;
 			m_Distance = 1.0f;
 		}
 	}
 
 	glm::vec3 EditorCamera::GetUpDirection() const
 	{
-		// y 축 회전
+		//  glm::vec3(0.0f, 1.0f, 0.0f)) : 최초 up direction
+		// 최초 up 축을 기준으로 x,y 축으로 얼만큼 회전했는가에 대한 값이다.
 		return glm::rotate(GetOrientation(), glm::vec3(0.0f, 1.0f, 0.0f));
 	}
 
@@ -132,17 +218,42 @@ namespace Hazel {
 
 	glm::vec3 EditorCamera::GetForwardDirection() const
 	{
-		// z 축 회전 (그런데 반대 방향..?)
+		/*
+		1) glm::vec3(0.0f, 0.0f, -1.0f)
+		- 현재 프로젝트 상에서 카메라의 Initiali 방향으로 보인다.
+		- OpenGL 은 오른손 좌표계이고, Z 방향이 우리 안쪽 방향이다
+		- 그런데 카메라는 정면을 바라보기 때문에, -1 * Z 방향이
+		  최초 카메라가 바라보는 방향이다.
+		- 일종의 회전 기준을 마련해주는 것으로 보인다.
+
+		2) glm::rotate
+		- quarternion에 명시된 회전값을 vec3 형태로 변환해서
+		  리턴해준다.
+
+		*/
 		return glm::rotate(GetOrientation(), glm::vec3(0.0f, 0.0f, -1.0f));
 	}
 
 	glm::vec3 EditorCamera::CalculatePosition() const
 	{
+		/*
+		ex) m_FocalPoint 가 (0,0,0), Distance 가 10 이면
+		Editor Camera 의 World Pos 는 사실상 z 축 -10 이 되고
+		이 말은 즉슨 z = 0 보다 조금 뒤에서 World 에 있는
+		대상들을 바라보게 된다는 의미가 된다.
+		*/
 		return m_FocalPoint - GetForwardDirection() * m_Distance;
 	}
 
 	glm::quat EditorCamera::GetOrientation() const
 	{
+		/*
+		1) 먼저 카메라의 Pitch, Yaw 값을 이용해서 3D Vector 를 만드는 것 같다.
+		- 회전값들을 '-' 해서 만들어준다는 것이 핵심이다
+		- 왜 '-' 를 해주는가는 잘 모르겠다. 
+		2) glm::quat 함수를 이용해서 3d vector 를
+		quarternion 형태로 변환한다.
+		*/
 		return glm::quat(glm::vec3(-m_Pitch, -m_Yaw, 0.0f));
 	}
 
--- HazelGameEngine/src/Renderer/EditorCamera.h
+++ HazelGameEngine/src/Renderer/EditorCamera.h
@@ -28,8 +28,16 @@ namespace Hazel {
 
 		glm::vec3 GetUpDirection() const;
 		glm::vec3 GetRightDirection() const;
+		/*
+		World 상에서 원점 기준으로
+		카메라가 바라보는 방향 벡터값을 리턴한다.
+		*/
 		glm::vec3 GetForwardDirection() const;
 		const glm::vec3& GetPosition() const { return m_Position; }
+
+		/*
+		단순히 x,y 회전값이 얼마인지
+		*/
 		glm::quat GetOrientation() const;
 
 		float GetPitch() const { return m_Pitch; }
@@ -40,27 +48,83 @@ namespace Hazel {
 
 		bool OnMouseScroll(MouseScrolledEvent& e);
 
+		/*
+		정리
+		1) MousePan, MouseZoom 은 간단하게 말하면
+		Camera 의 Transition 을 이동시키는 것 
+		2) MouseRotate 는 
+		Camera 의 Rotation 을 변경시키는 것
+
+		각각을 Transition, Rotate 로 고려해서
+		World 변환 행렬 S,R,T 를 만드는 것이다.
+		*/
+		// 마우스를 이동시키는 함수
 		void MousePan(const glm::vec2& delta);
+		// 마우스를 회전시키는 함수
 		void MouseRotate(const glm::vec2& delta);
+		// 마우스 줌인을 해주는 함수
 		void MouseZoom(float delta);
 
 		glm::vec3 CalculatePosition() const;
 
 		std::pair<float, float> PanSpeed() const;
 		float RotationSpeed() const;
+
+		/*
+		Mouse Scroll 하면서 Camera 의 Zoom Speed 를
+		조절해주는 함수
+
+		z = 0 에 가까울 수록, zoom speed 는 줄어들어서
+		천천히 가까워지고
+
+		z = 0 으로부터 멀수록, zoom speed 는 빨라져서
+		빨리 가까워진다.
+		*/
 		float ZoomSpeed() const;
 	private:
 		float m_FOV = 45.0f, m_AspectRatio = 1.778f, m_NearClip = 0.1f, m_FarClip = 1000.0f;
 
 		glm::mat4 m_ViewMatrix;
+		
+		/*
+		World 상에서 Camera 의 Pos 라고 생각하면 된다.
+		Translation 정보
+		*/
 		glm::vec3 m_Position = { 0.0f, 0.0f, 0.0f };
+
+		/*
+		Focal point 라는 것의 의미는, 해당 지점
+		beyond 로 넘어갈 수 없다는 것을 의미한다.
+
+		쉽게 말해 Camera 가 Focusing 하는 중앙 지점이라고
+		생각해도 된다.
+
+		MousePan 함수에서 Focal Point 를 이동시킨다.
+		Camera 의 특정 Position  을 이동시키는 개념이 아니다.
+		*/
 		glm::vec3 m_FocalPoint = { 0.0f, 0.0f, 0.0f };
 
 		glm::vec2 m_InitialMousePosition = { 0.0f, 0.0f };
 
+		/*
+		Scene 의 원점 (0,0,0) 을 focusing 하면서
+		동시에 조금 뒤에서 해당 원점을 바라보게 하는 것이다.
+
+		z 가 1보다 작지 않게 조절한다.
+		즉, 너무 앞으로 가서 물체들 뒤로 넘어가는 것을
+		방지하기 위한 것 같기도 하다.
+		*/
 		float m_Distance = 10.0f;
-		// Pitch : 카메라 방향의 x 축 (옆)
-		// Yaw  : 카메라 방향의 y 축 (위)
+
+		/*
+		Pitch : 카메라 방향의 x 축 (위, 아래) 회전 각도
+		Yaw  : 카메라 방향의 y 축 (왼, 오) 회전 각도
+		z 축으로의 회전은 방지하는 것으로 보인다.
+
+		OPENGL 오른손 좌표계에서는
+		각 축을 기준으로, 반시계 방향이
+		양수 회전값이라고 생각하면 된다.
+		*/
 		float m_Pitch = 0.0f, m_Yaw = 0.0f;
 
 		float m_ViewportWidth = 1280, m_ViewportHeight = 720;
