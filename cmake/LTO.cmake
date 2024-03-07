cb4343079cf93f8586e7a7dd60f7c3f7efb75883
ohbumjun <beomjun0638@hanmail.net>
2024-03-07 22:16:20 +0900

    feat(CMake) rollback glad to seperate project

--- cmake/LTO.cmake
+++ cmake/LTO.cmake
@@ -1,10 +1,12 @@
 function(target_enable_lto)
     # two parameters : TARGET and ENABLE
     # two parameters be in 'oneValueArgs'
+    # oneValueArgs : specify that both parameters are expected to have single value 
     set(oneValueArgs TARGET ENABLE)
 
-    # 1st param : prefix ex) LTO
+    # cmake_parse_arguments : pase command-line arguments passed to function
     # in this case. every value set in 'onValueArgs' will be prefixed by 'LTO_
+    # ex) LTO_TARGET, LTO_ENABLE created
     cmake_parse_arguments(
         LTO
         "${options}"
@@ -15,13 +17,19 @@ function(target_enable_lto)
     # predefined cmake module
     # can check lto is supported or not
     include(CheckIPOSupported)
+
     check_ipo_supported(RESULT result OUTPUT output)
+
     if(result)
         message(STATUS "IPO/LTO is supported: ${LTO_TARGET}")
         # INTERPROCEDURAL_OPTIMIZATION : predefined option by cmakelist
         # below : enable lto for certain target
-        set_property(TARGET ${LTO_TARGET} PROPERTY INTERPROCEDURAL_OPTIMIZATION
-                                                   ${LTO_ENABLE})
+        set_property(
+            TARGET ${LTO_TARGET} 
+            PROPERTY 
+            INTERPROCEDURAL_OPTIMIZATION
+            ${LTO_ENABLE}
+        )
     else()
         message(WARNING "IPO/LTO is not supported: ${LTO_TARGET}")
     endif()
