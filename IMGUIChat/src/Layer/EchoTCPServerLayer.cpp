997821c1b8db5d278d5104cd57c2666536e74992
ohbumjun <beomjun0638@hanmail.net>
2024-03-30 23:29:51 +0900

    feat(ImguiChat) Create New Process after click 'CreateClient' in Imgui

--- IMGUIChat/src/Layer/EchoTCPServerLayer.cpp
+++ IMGUIChat/src/Layer/EchoTCPServerLayer.cpp
@@ -173,44 +173,41 @@ void EchoTCPServerLayer::ImGuiCreateClientWindow()
 
 void EchoTCPServerLayer::createClient()
 {
-    if (ImGui::Button("CreateClient", ImVec2(100, 0)))
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    const Hazel::ApplicationSpecification &specification =
+        Hazel::Application::Get().GetSpecification();
+
+    const char *constCharExecutablePath = specification.CommandLineArgs[0];
+    LPSTR executablePath = _strdup(constCharExecutablePath);
+
+    // Start the child process.
+    if (!CreateProcess(NULL, // No module name (use command line)
+                        executablePath, // Command line
+                        NULL,  // Process handle not inheritable
+                        NULL,  // Thread handle not inheritable
+                        FALSE, // Set handle inheritance to FALSE
+                        0,     // No creation flags
+                        NULL,  // Use parent's environment block
+                        NULL,  // Use parent's starting directory
+                        &si,   // Pointer to STARTUPINFO structure
+                        &pi)   // Pointer to PROCESS_INFORMATION structure
+    )
     {
-        STARTUPINFO si;
-        PROCESS_INFORMATION pi;
-
-        ZeroMemory(&si, sizeof(si));
-        si.cb = sizeof(si);
-        ZeroMemory(&pi, sizeof(pi));
-
-        const Hazel::ApplicationSpecification &specification =
-            Hazel::Application::Get().GetSpecification();
-
-        const char *constCharExecutablePath = specification.CommandLineArgs[0];
-        LPSTR executablePath = _strdup(constCharExecutablePath);
-
-        // Start the child process.
-        if (!CreateProcess(NULL, // No module name (use command line)
-                           executablePath, // Command line
-                           NULL,  // Process handle not inheritable
-                           NULL,  // Thread handle not inheritable
-                           FALSE, // Set handle inheritance to FALSE
-                           0,     // No creation flags
-                           NULL,  // Use parent's environment block
-                           NULL,  // Use parent's starting directory
-                           &si,   // Pointer to STARTUPINFO structure
-                           &pi)   // Pointer to PROCESS_INFORMATION structure
-        )
-        {
-            printf("CreateProcess failed (%d).\n", GetLastError());
-            return;
-        }
-
-        free(executablePath);
-
-        // Close process and thread handles.
-        CloseHandle(pi.hProcess);
-        CloseHandle(pi.hThread);
+        printf("CreateProcess failed (%d).\n", GetLastError());
+        return;
     }
+
+    free(executablePath);
+
+    // Close process and thread handles.
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
 }
 
 void EchoTCPServerLayer::initializeConnection()
